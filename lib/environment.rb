b222920e4490e9af2d793ddba3680d1b611e7fd6
Landon Reasoner <reasoner.landon90@gmail.com>
2020-08-30 02:26:01 +0000

    lib/environment.rb

--- lib/environment.rb
+++ lib/environment.rb
@@ -1,10 +1,10 @@
 require 'pry'
 require 'httparty'
 
-require_relative "crypto_prices/version"
-require_relative "crypto_prices/cli"
-require_relative "crypto_prices/api"
-require_relative "crypto_prices/crypto"
+require_relative "./crypto_prices/version"
+require_relative "./crypto_prices/cli"
+require_relative "./crypto_prices/api"
+require_relative "./crypto_prices/crypto"
 
 module CryptoPrices
   class Error < StandardError; end
