3638a9c605d70e6ec8c17a406f6a5f94386830b9
Landon Reasoner <reasoner.landon90@gmail.com>
2021-01-04 22:50:09 +0000

    lib/crypto_prices/api.rb

--- lib/crypto_prices/api.rb
+++ lib/crypto_prices/api.rb
@@ -3,6 +3,10 @@ require 'HTTParty'
 
 class CryptoPrices::API 
   
+  def self.get_pairs
+    HTTParty.get("https://api.coinbase.com/v2/currencies")
+  end 
+  
   def self.get_price
     HTTParty.get("https://api.coinbase.com/v2/prices/:currency_pair/spot")
   end
