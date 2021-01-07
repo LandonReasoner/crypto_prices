19508585bfee42ae6612fb3b2c80dbba770ef4c8
Landon Reasoner <reasoner.landon90@gmail.com>
2021-01-07 22:32:35 +0000

    lib/crypto_prices/api.rb

--- lib/crypto_prices/api.rb
+++ lib/crypto_prices/api.rb
@@ -3,12 +3,12 @@ require 'httparty'
 
 class CryptoPrices::API 
   
-  def self.get_status
-    HTTParty.get("https://api.pro.coinbase.com/products/btc-usd/stats")
-  end 
-  
-  def self.get_price
+  def self.get_pairs
     HTTParty.get("https://api.pro.coinbase.com/products")
   end
-  binding.pry
+  
+  def self.get_status
+    HTTParty.get("https://api.pro.coinbase.com/products/#{@options[:symbol]}/stats")
+  end 
+  
 end
