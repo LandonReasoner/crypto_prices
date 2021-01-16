ce36d8a618319752604bdbcd5615728d96b8b88f
Landon Reasoner <reasoner.landon90@gmail.com>
2021-01-16 16:24:56 +0000

    lib/crypto_prices/api.rb

--- lib/crypto_prices/api.rb
+++ lib/crypto_prices/api.rb
@@ -3,19 +3,19 @@ require 'httparty'
 
 class CryptoPrices::API 
   
-  def get_pairs
+  def self.get_pairs
     pairs = HTTParty.get("https://api.pro.coinbase.com/products")
     parsed(pairs)
   end
   
-  def parsed(pairs)
-    pairs.each do |pairs|
-      cryptos = pairs.dig([],:id)
-    end 
+  
+    #addresses.map { |address| address.dig(:location, :latitude) }
+  def self.parsed(pairs)
+    pairs.map{ |pair| pair.dig([],:id) }
   end
   
   binding.pry
-  def get_status
+  def self.get_status
     HTTParty.get("https://api.pro.coinbase.com/products/#{@options[:pair]}/stats")
   end 
   
